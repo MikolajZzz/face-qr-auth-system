a7151d7bb6c57279db5a4d71948ce0169cae0234
MikolajZzz <zajdekmikolaj@gmail.com>
2026-01-19 01:12:44 +0100

    Dodanie Readme z instrukcją uruchomienia aplikacji

--- run.ps1
+++ run.ps1
@@ -2,7 +2,7 @@ Write-Host "=== Aktywacja virtualenv ==="
 . .\.venv\Scripts\Activate.ps1
 
 Write-Host "=== Instalacja requirements.txt ==="
-pip install -r requirements.txt --quiet
+pip install -r requirements.txt --quiet 
 
 Write-Host "=== Uruchamianie aplikacji ==="
 python -m backend.app
